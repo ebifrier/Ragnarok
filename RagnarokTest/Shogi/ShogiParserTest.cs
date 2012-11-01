3fd9765b916e64c2ed365cbc1d8c519f4904b8ee
ebifrier <ebifrier@hotmail.co.jp>
2012-11-01 21:10:45 +0900

    Ragnarok/Utility/StringNormalizer.cs: ラ行の文字正規化が行われていなかったため、修正しました。

--- RagnarokTest/Shogi/ShogiParserTest.cs
+++ RagnarokTest/Shogi/ShogiParserTest.cs
@@ -22,12 +22,17 @@ namespace RagnarokTest.Shogi
                     Piece = Piece.Kaku,
                 });
 
+            var gin68 = new Move
+            {
+                NewPosition = new Position(6, 8),
+                Piece = Piece.Gin,
+            };
             Assert.AreEqual(
                 ShogiParser.ParseMove("ろっぱち銀", true),
-                new Move {
-                    NewPosition = new Position(6, 8),
-                    Piece = Piece.Gin,
-                });
+                gin68);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("ロッパチ銀", true),
+                gin68);
         }
     }
 }
