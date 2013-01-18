a8980e68a2533a89b3185e900f8f4d76883b79d2
ebifrier <ebifrier@hotmail.co.jp>
2013-01-19 03:47:12 +0900

    Ragnarok/Update/make_release.rb: 更新ファイル作成用のファイルを追加しました。

---
+++ Ragnarok/Update/make_release.rb
@@ -0,0 +1,330 @@
+#!/usr/local/bin/ruby -Ku
+#
+# Ragnarok.Updateに使う更新ファイルなどを作成します。
+#
+
+require 'rubygems'
+require 'yaml'
+require 'fileutils'
+require 'image_size'
+require 'net/ftp'
+require 'digest/md5'
+require 'rexml/document'
+
+#$HtmlImagePath = "http://garnet-alice.net/common/images/"
+$HtmlImagePath = "E:/Dropbox/NicoNico/homepage/public_html/common/images"
+
+#
+# ファイル又はディレクトリを削除するメソッドを作成
+#
+def deleteall(delthem)
+  if FileTest.directory?(delthem) then  # ディレクトリかどうかを判別
+    Dir.foreach( delthem ) do |file|    # 中身を一覧
+      next if /^\.+$/ =~ file           # 上位ディレクトリと自身を対象から外す
+      deleteall(delthem.sub(/\/+$/,"") + "/" + file)
+    end
+    Dir.rmdir(delthem) rescue ""        # 中身が空になったディレクトリを削除
+  else
+    if File.exists?(delthem)
+      File.delete(delthem)              # ディレクトリでなければ削除
+    end
+  end
+end
+
+#
+# ファイルのMD5を計算します。
+#
+def compute_md5(filename)
+  # binary mode必須
+  File.open(filename, "rb") do |f|
+    s = f.read
+    
+    return Digest::MD5.hexdigest(s)
+  end
+end
+
+#
+# サーバー上のサムネを一つ、バージョンと日付から選択します。
+#
+def select_thumbnail(rdata)
+  return rdata.thumbnail if rdata.thumbnail != nil
+  
+  image_path = File.join($HtmlImagePath, "*")
+  filelist = Dir.glob(image_path).
+    map do |path| File.basename(path) end.
+    sort
+    
+  # md5から一意な画像を選択します。
+  md5 = Digest::MD5.new()
+  md5.update(rdata.version)
+  md5.update(rdata.date)
+  message = md5.digest
+  
+  digest = 0
+  message.bytes do |b|
+    digest += b
+  end
+  
+  filelist[digest % filelist.length]
+end
+
+#
+# サーバー上のサムネイルのフルパスを取得します。
+#
+def make_thumbnail_fullpath(filename)
+  File.join($HtmlImagePath, filename)
+end
+
+#
+# 画像のサイズを取得します。
+#
+def get_image_size(filename)
+  image_path = make_thumbnail_fullpath(filename)
+  
+  File::open(image_path, "rb") do |f|
+    return ImageSize.new(f.read)
+  end
+end
+
+#
+# 各リリース情報を保持します。
+#
+class ReleaseData
+  attr_accessor :version, :thumbnail, :date, :content
+
+  def initialize(node)
+    @version = node["version"]
+
+    @thumbnail = node["thumbnail"]
+    @date = node["date"]
+    @content = node["content"]
+  end
+
+  # infoタグをhtml表示用に変換したcontentを取得します。
+  def html_content()
+    root = REXML::Element.new("ul")
+
+    @content.each do |info_node|
+      new_node = REXML::Element.new("li")
+
+      if info_node.instance_of?(String)
+        # 文字列のみの場合は、それを直接設定。
+        new_node.text = info_node
+      else
+        pair = info_node.first
+
+        # infoのタイプ名です。
+        text = "(" + pair[0] + ") "
+
+        if pair[0] == "本体"
+          new_node.add_element("span", {"class" => "main"}).add_text(text)
+        elsif pair[0] == "将棋"
+          new_node.add_element("span", {"class" => "shogi"}).add_text(text)
+        else
+          new_node.add_element("span", {"class" => "unknown"}).add_text(text)
+        end
+        
+        new_node.add_text(pair[1])
+      end
+      root.add_element(new_node)
+    end
+    
+    root
+  end
+end
+
+#
+# アプリのタイトルやバージョン情報を保持します。
+#
+# dist_path上にビルドファイル、zip、更新ファイルなどを作成します。
+#
+class AppData
+  attr_accessor :html_dir, :dist_path, :title, :version, :release_list
+  attr_accessor :outdir_name, :outdir_path, :zip_name, :zip_path
+  attr_accessor :versioninfo_path, :releasenote_path
+  
+  def initialize(html_dir, dist_path, assemblyinfo_path, history_path)
+    @html_dir = html_dir
+    @title = load_title(assemblyinfo_path)
+    @version = load_version(assemblyinfo_path)
+    @release_list = load_history(history_path) if history_path != nil
+
+    @dist_path = dist_path
+    @outdir_name = "#{@title}_#{version_}"
+    @outdir_path = File.join(dist_path, outdir_name)
+    @zip_name = outdir_name + ".zip"
+    @zip_path = File.join(dist_path, zip_name)
+    
+    @versioninfo_path = File.join(dist_path, "versioninfo.xml")
+    @releasenote_path = File.join(dist_path, "release_note.html")
+  end
+
+  #
+  # _で区切られたバージョン情報を取得します。
+  #
+  def version_()
+    @version.gsub(".", "_")
+  end
+
+  #
+  # クライアントのタイトルを取得します。
+  #
+  def load_title(assemblyinfo_path)
+    str = File.open(assemblyinfo_path).read
+  
+    if /\[assembly: AssemblyTitle\("(.+)"\)\]/ =~ str then
+      version = $1
+    else
+      puts "Not found client's assembly title."
+      exit(-1)
+    end
+  end
+  
+  #
+  # クライアントのバージョンを取得します。
+  #
+  def load_version(assemblyinfo_path)
+    str = File.open(assemblyinfo_path).read
+  
+    if /\[assembly: AssemblyVersion\("([\d\.]+)"\)\]/ =~ str then
+      version = $1
+    else
+      puts "Not found client's assembly version."
+      exit(-1)
+    end
+  end
+  
+  #
+  # 更新履歴を読み込みます。
+  #
+  def load_history(history_path)
+    history = YAML.load_file(history_path)
+
+    history.map do |node|
+      ReleaseData.new(node)
+    end
+  end
+  
+  #
+  # プロジェクトのリビルドを行います。
+  #
+  def build(solution_path, constants)
+    # ディレクトリが存在したら削除します。
+    if File.exists?(@outdir_path)
+      deleteall(@outdir_path)
+    end
+    
+    # ビルドコマンドを実行します。
+    path_command = "call \"%VS100COMNTOOLS%vsvars32.bat\""
+    build_command =
+      "msbuild /nologo \"#{solution_path}\" /t:Rebuild " +
+      "/p:DefineConstants=\"#{constants}\" " +
+      "/p:OutputPath=\"#{@outdir_path}\" /p:Configuration=Release"
+    
+    puts build_command
+    system(path_command + " && " + build_command)
+  end
+  
+  #
+  # templateファイルを置き換えます。
+  #
+  def convert_template(input_path, output_path)
+    data = File.open(input_path).read
+    data = data.gsub("${DIRNAME}", @html_dir)
+    data = data.gsub("${VERSION}", @version)
+    data = data.gsub("${_VERSION}", version_)
+    data = data.gsub("${ZIP_FILE}", @zip_name)
+    data = data.gsub("${MD5}", compute_md5(@zip_name))
+    data = data.gsub("${PUB_DATE}", Time.now.to_s)
+    data = data.gsub("${RAND}", rand(100000).to_s)
+    
+    File.open(output_path, "w") do |f|
+      f.write(data)
+      printf("wrote %s\n", output_path)
+    end
+  end
+  
+  #
+  # zipファイルを作成します。
+  #
+  def make_zip()
+    deleteall(@zip_path)
+    
+    current = Dir.pwd
+    Dir.chdir(@outdir_path)
+    filelist = Dir.glob("*").map do |name|
+      '"' + name + '"'
+    end
+    files_str = filelist.join(' ')
+    
+    zip_command = "zip -r -q \"#{@zip_path}\" " + files_str
+    printf("begin command: %s\n", zip_command)
+    system(zip_command)
+    
+    Dir.chdir(current)
+  end
+  
+  #
+  # versioninfo.xmlを更新します。
+  #
+  def make_versioninfo(tmpl_path)
+    convert_template(tmpl_path, @versioninfo_path)
+  end
+
+  #
+  # リリース情報(html)を出力します。
+  #
+  def make_release_note(tmpl_path)
+    # release_note.html のテンプレートファイルを読み込みます。
+    fp = File.open(tmpl_path, "r")
+    doc = REXML::Document.new(fp)
+    
+    top = doc.elements["//body"]
+    rnote_templ = top.elements["//div[@class=\"versioninfo\"]"]
+    
+    # delete_allは実体を消すためコピーが必要になります。
+    rnote_templ = rnote_templ.deep_clone
+    top.elements.delete_all("*")
+    
+    @release_list.each_with_index do |rdata, i|
+      break if i >= 3
+      note_node = rnote_templ.deep_clone
+      
+      if i == 0 and rdata.version != @version
+        raise StandardError, 'Release version isn\'t matched (history.xml)'
+      end
+      
+      # header
+      node = note_node.elements["//*[@class=\"version\"]"]
+      node.text = "更新履歴　#{@title} #{rdata.version}"
+      
+      # thumbnail
+      node = note_node.elements["//*[@class=\"thumbnail\"]"]
+      filename = select_thumbnail(rdata)
+      image_size = get_image_size(filename)
+      attrs = {
+        'src' => make_thumbnail_fullpath(filename),
+        'alt' => '更新情報',
+        'border' => '0',
+        'width' => image_size.width,
+        'height' => image_size.height,
+      }
+      node.add_element("img", attrs)
+      select_thumbnail(rdata)
+      
+      # date
+      node = note_node.elements["//*[@class=\"date\"]"]
+      node.text = (rdata.date ? "更新日: " + rdata.date : " ")
+      
+      # content
+      node = note_node.elements["//*[@class=\"content\"]"]
+      node.add_element(rdata.html_content)
+      
+      top.add_element(note_node)
+    end
+    
+    File.open(@releasenote_path, "w") do |f|
+      doc.write(f, -1, false, true)
+    end
+  end
+end
