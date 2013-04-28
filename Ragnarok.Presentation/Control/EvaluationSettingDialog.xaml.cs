f265995c7e20f4ff4c8eb6b5c57ba0a82e4eecc2
ebifrier <ebifrier@hotmail.co.jp>
2013-04-28 11:38:10 +0900

    Ragnarok.Presentation/Control/EvaluationControl.cs: 評価値表示用コントロールを追加しました。

---
+++ Ragnarok.Presentation/Control/EvaluationSettingDialog.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using Ragnarok.ObjectModel;
+using Ragnarok.Presentation.Utility;
+
+namespace Ragnarok.Presentation.Control
+{
+    /// <summary>
+    /// EvaluationSettingDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class EvaluationSettingDialog : Window
+    {
+        private CloneModel model;
+        private EvaluationControl control;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public EvaluationSettingDialog(EvaluationControl control)
+        {
+            InitializeComponent();
+            CommandBindings.Add(
+                new CommandBinding(
+                    RagnarokCommands.OK,
+                    ExecuteYes));
+
+            this.model = new CloneModel(control);
+            this.control = control;
+
+            DataContext = model;
+        }
+
+        /// <summary>
+        /// OK/YES
+        /// </summary>
+        private void ExecuteYes(object sender, ExecutedRoutedEventArgs e)
+        {
+            // OKの場合は、プロパティ値をコントロールに設定します。
+            this.model.SetValuesToTarget(this.control);
+
+            DialogResult = true;
+        }
+    }
+}
---
+++ Ragnarok.Presentation/Utility/ImageSetInfo.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using Ragnarok.Utility;
+
+namespace Ragnarok.Presentation.Utility
+{
+    /// <summary>
+    /// 評価値ウィンドウ用の画像セット情報を保持します。
+    /// </summary>
+    [DataContract()]
+    public class ImageSetInfo : InfoBase
+    {
+        /// <summary>
+        /// 評価値ウィンドウに使う各画像の情報を保持します。
+        /// </summary>
+        [DataContract()]
+        internal class InternalImage
+        {
+            /// <summary>
+            /// 画像が適用される評価値の最小値を取得または設定します。
+            /// </summary>
+            [DataMember(Name = "min")]
+            public double MinValue
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// 画像が適用される評価値の最大値を取得または設定します。
+            /// </summary>
+            [DataMember(Name = "max")]
+            public double MaxValue
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// 画像のパスを取得または設定します。
+            /// </summary>
+            [DataMember(Name = "image")]
+            public string ImagePath
+            {
+                get;
+                set;
+            }
+
+            /// <summary>
+            /// コンストラクタ
+            /// </summary>
+            public InternalImage()
+            {
+                // デフォルトは無限小＆無限大です。
+                MinValue = int.MinValue;
+                MaxValue = int.MaxValue;
+            }
+        }
+
+        /// <summary>
+        /// 画像リストを取得します。
+        /// </summary>
+        [DataMember(Name = "imageList")]
+        private List<InternalImage> ImageList
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 評価値から該当する画像ファイルのパスを取得します。
+        /// </summary>
+        public string GetSelectedImagePath(double point)
+        {
+            var selectedImage = ImageList.FirstOrDefault(
+                info => info.MinValue <= point && point <= info.MaxValue);
+
+            return (selectedImage != null
+                ? Path.Combine(BasePath, selectedImage.ImagePath)
+                : null);
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ImageSetInfo()
+        {
+            ImageList = new List<InternalImage>();
+        }
+    }
+}
