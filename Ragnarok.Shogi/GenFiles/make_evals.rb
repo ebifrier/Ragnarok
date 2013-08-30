48cada1b12142efcfa2c59a454e0c2cd3e6c3f7f
ebifrier <devnull@localhost>
2013-08-30 20:37:13 +0900

    *.csproj: プロジェクトのプラットフォームをx86に変更しました。 Ragnarok.Shogi/SampleMove.cs: 指し手サンプルに局面の評価値を含められるようにしました。

--- Ragnarok.Shogi/GenFiles/make_evals.rb
+++ Ragnarok.Shogi/GenFiles/make_evals.rb
@@ -10,19 +10,32 @@ require 'kconv'
 #
 def load_evals_from_kifu(filepath)
   File.open(filepath) do |f|
-    f.map {|line| /\s*\*<analyze>([-]?\d+)\s*(.*)\s*<\/analyze>/ =~ line; $1}.
-    select {|str| str != nil}.
-    map {|str| str.to_i}
+    str = f.read
+    
+    result = []
+    while /\s*\*<analyze>([-]?\d+)\s*(.*)\s*<\/analyze>\s+(\d+)\s+(\S+)\s+/ =~ str
+      str = $'
+      next if $1 == nil
+      
+      result << [$4, $1.to_i, $3.to_i]
+    end
+    result
   end
 end
 
-# 
-filepath = "kifu.kif"
-value_list = load_evals_from_kifu(filepath)
-
-fp = File.open("kifu_eval.txt", "w")
-value_list.each_with_index do |v, i|
-  fp.printf("% 7d,", v)
-  fp.printf("\n") if i % 5 == 4
+begin
+  filepath = "kifu.kif"
+  value_list = load_evals_from_kifu(filepath)
+  
+  fp = File.open("kifu_eval.txt", "w")
+  value_list.each_with_index do |a, i|
+    fp.printf("M(\"%s%s\", %d),",
+      (a[2] % 2 != 0 ? "£" : "¢"), a[0], a[1])
+    fp.printf(" ") if i % 2 == 0
+    fp.printf("\n") if i % 2 == 1
+  end
+  fp.close
+rescue => e
+  puts "#{e.message}\n#{e.backtrace}"
 end
-fp.close
+#gets
