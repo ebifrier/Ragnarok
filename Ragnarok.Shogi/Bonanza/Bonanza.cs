cea907e5b9115577bcdbf3ecbdd8189c7f8c4285
ebifrier <ebifrier@gmail.com>
2014-05-01 13:36:59 +0900

    PluginShogi/Effects/EffectManager.cs: 投票エフェクトが一度しか出ないバグを修正しました。

--- Ragnarok.Shogi/Bonanza/Bonanza.cs
+++ Ragnarok.Shogi/Bonanza/Bonanza.cs
@@ -87,11 +87,6 @@ namespace Ragnarok.Shogi.Bonanza
         public event EventHandler<BonanzaReceivedCommandEventArgs> ErrorReceived;
 
         /// <summary>
-        /// 初期化が終わった時に呼ばれます。
-        /// </summary>
-        public event EventHandler MnjInited;
-
-        /// <summary>
         /// ボナンザが終了したときに呼ばれます。
         /// </summary>
         public event EventHandler<BonanzaAbortedEventArgs> Aborted;
@@ -101,7 +96,6 @@ namespace Ragnarok.Shogi.Bonanza
         /// </summary>
         public Bonanza()
         {
-            CommandReceived += Bonanza_ReceivedCommand;
             ErrorReceived += Bonanza_ReceivedError;
         }
 
@@ -145,15 +139,6 @@ namespace Ragnarok.Shogi.Bonanza
         {
             get { return GetValue<bool>("IsConnected"); }
             set { SetValue("IsConnected", value); }
-        }
-
-        /// <summary>
-        /// mnjinitコマンドが完了したかどうかを取得または設定します。
-        /// </summary>
-        public bool? IsMnjInited
-        {
-            get { return GetValue<bool?>("IsMnjInited"); }
-            set { SetValue("IsMnjInited", value); }
         }
 
         /// <summary>
@@ -318,31 +303,6 @@ namespace Ragnarok.Shogi.Bonanza
         }
 
         #region コールバック
-        /// <summary>
-        /// mnjprepareの結果を識別する正規表現です。
-        /// </summary>
-        private static readonly Regex MnjPrepareRegex = new Regex(
-            @"^info mnjprepare\s*([\w]+)\s*$",
-            RegexOptions.IgnoreCase);
-
-        void Bonanza_ReceivedCommand(object sender, BonanzaReceivedCommandEventArgs e)
-        {
-            if (IsMnjInited == null)
-            {
-                var m = MnjPrepareRegex.Match(e.Command);
-                if (m.Success)
-                {
-                    var inited = (m.Groups[1].Value == "ok");
-
-                    IsMnjInited = inited;
-                    if (inited)
-                    {
-                        MnjInited.SafeRaiseEvent(this, EventArgs.Empty);
-                    }
-                }
-            }
-        }
-
         void Bonanza_ReceivedError(object sender, BonanzaReceivedCommandEventArgs e)
         {
             var error = e.Command;
@@ -367,18 +327,6 @@ namespace Ragnarok.Shogi.Bonanza
 
         #region mnj
         /// <summary>
-        /// mnjprepare処理を開始します。
-        /// </summary>
-        public void BeginPrepareMnj()
-        {
-            var command = string.Format(
-                "mnjprepare 15 {0}",
-                MathEx.RandInt());
-
-            WriteCommand(command);
-        }
-
-        /// <summary>
         /// 並列化サーバーに接続します。
         /// </summary>
         public void Connect(string serverAddress, int serverPort, int dfpnPort,
@@ -391,12 +339,6 @@ namespace Ragnarok.Shogi.Bonanza
                 {
                     throw new InvalidOperationException(
                         "ボナンザが起動していません。");
-                }
-
-                if (IsMnjInited != true)
-                {
-                    throw new InvalidOperationException(
-                        "mnjが初期化されていません。");
                 }
 
                 if (IsConnected)
