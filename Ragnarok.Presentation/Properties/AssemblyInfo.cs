a481d45924cef7ea0099c6bd330dd40295a40491
ebifrier <ebifrier@gmail.com>
2014-06-03 00:02:51 +0900

    Ragnarok/Ragnarok.csproj: ビルド定数を修正しました。 Ragnarok.Presentation/Ragnarok.Presentation.csproj: ファイルをRagnarok.Presentation.Extraに移動しました。 Ragnarok.Presentation/Properties/AssemblyInfo.cs: 不要になったネームスペースを削除しました。

--- Ragnarok.Presentation/Properties/AssemblyInfo.cs
+++ Ragnarok.Presentation/Properties/AssemblyInfo.cs
@@ -38,15 +38,12 @@ using System.Windows.Markup;
 )]
 
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation")]
-[assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.Behaviors")]
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.Command")]
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.Control")]
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.Converter")]
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.Extension")]
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.NicoNico")]
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.Utility")]
-[assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.VisualObject")]
-[assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.VisualObject.Control")]
 
 // アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
 //
