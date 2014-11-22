c7bce2e6aaf6555bb79f7b2e826c4f28c688ec76
ebifrier <ebifrier@gmail.com>
2014-11-22 22:07:51 +0900

    Ragnarok.Forms/Draw/BitmapExtension.cs: Bitmapクラス用の操作メソッドを追加しました。 Ragnarok.Extra/FlintSharp/Counters/PerformanceAdjusted.cs: 不要なファイルを削除しました。

---
+++ Ragnarok.Forms/Draw/BitmapExtension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+
+namespace Ragnarok.Forms.Draw
+{
+    /// <summary>
+    /// Bitmapクラス用の拡張メソッドを実装します。
+    /// </summary>
+    public static class BitmapExtension
+    {
+        /// <summary>
+        /// 画像を綺麗に拡大縮小します。
+        /// </summary>
+        public static Bitmap ResizeHighQuality(this Bitmap bitmap,
+                                               int width, int height)
+        {
+            if (bitmap == null)
+            {
+                throw new ArgumentNullException("bitmap");
+            }
+
+            var target = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                g.DrawImage(bitmap, 0, 0, width, height); 
+            }
+
+            return target;
+        }
+
+        /// <summary>
+        /// 画像の一部を綺麗に切り抜きます。
+        /// </summary>
+        public static Bitmap CropHighQuality(this Bitmap bitmap, int x, int y,
+                                             int width, int height)
+        {
+            if (bitmap == null)
+            {
+                throw new ArgumentNullException("bitmap");
+            }
+
+            var target = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                g.DrawImage(bitmap,
+                    new Rectangle(0, 0, width, height), // dst
+                    new Rectangle(x, y, width, height), // src
+                    GraphicsUnit.Pixel);
+            }
+
+            return target;
+        }
+    }
+}
