49369aeb8ac38885ebb45a001a40e20f3d48fe43
ebifrier <ebifrier@gmail.com>
2014-11-28 02:24:55 +0900

    Ragnarok/Utility/DurationConverter.cs: Xaml上からForeverと指定することでTimeSpanのMaxValueにすることや、マイナスの値を許可しないなどの制約をつけたXamlコンバータを追加しました。

---
+++ Ragnarok/Utility/DurationConverter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Ragnarok.Utility
+{
+    /// <summary>
+    /// TimeSpanクラスの'Forever'による指定を可能にします。
+    /// </summary>
+    public sealed class DurationConverter : TypeConverter
+    {
+        /// <summary>
+        /// 他の値をTimeSpanに変換できるか調べます。
+        /// </summary>
+        public override bool CanConvertFrom(ITypeDescriptorContext context,
+                                            Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        /// <summary>
+        /// 文字列型をTimeSpan型に変換します。
+        /// </summary>
+        public override object ConvertFrom(ITypeDescriptorContext context,
+                                           CultureInfo culture, object value)
+        {
+            var str = value as string;
+            if (str != null)
+            {
+                if (str == "Forever")
+                {
+                    return TimeSpan.MaxValue;
+                }
+
+                var span = TimeSpan.Parse(str, culture);
+                if (span < TimeSpan.Zero)
+                {
+                    throw new FormatException(
+                        "DurationConverter使用時にTimeSpanの値がマイナスになりました。");
+                }
+
+                return span;
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        /// <summary>
+        /// TimeSpan型を変換できるか調べます。
+        /// </summary>
+        public override bool CanConvertTo(ITypeDescriptorContext context,
+                                          Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertTo(context, destinationType);
+        }
+
+        /// <summary>
+        /// TimeSpan型を文字列に変換します。
+        /// </summary>
+        public override object ConvertTo(ITypeDescriptorContext context,
+                                         CultureInfo culture, object value,
+                                         Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                return value.ToString();
+            }
+
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
+    }
+}
