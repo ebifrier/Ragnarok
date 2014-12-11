644555dd984a08b689c0aafc7ea629e373348b45
ebifrier <ebifrier@gmail.com>
2014-12-12 02:26:46 +0900

    Ragnarok.Forms/Controls/InfoControl.cs: 情報表示用のコントロールを追加しました。

---
+++ Ragnarok.Forms/Controls/InfoControl.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Ragnarok.Forms.Controls
+{
+    partial class InfoControl
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.itemNameLabel1 = new System.Windows.Forms.Label();
+            this.itemNameLabel2 = new System.Windows.Forms.Label();
+            this.itemNameLabel3 = new System.Windows.Forms.Label();
+            this.nameValueLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Controls.Add(this.nameLabel, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.itemNameLabel1, 0, 2);
+            this.tableLayoutPanel.Controls.Add(this.itemNameLabel2, 0, 4);
+            this.tableLayoutPanel.Controls.Add(this.itemNameLabel3, 0, 6);
+            this.tableLayoutPanel.Controls.Add(this.nameValueLabel, 1, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 7;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 6F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 6F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 6F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(266, 91);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.nameLabel.Location = new System.Drawing.Point(3, 0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(89, 18);
+            this.nameLabel.TabIndex = 4;
+            this.nameLabel.Text = "作者名 or 名前：";
+            // 
+            // itemNameLabel1
+            // 
+            this.itemNameLabel1.AutoSize = true;
+            this.itemNameLabel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.itemNameLabel1.Location = new System.Drawing.Point(30, 24);
+            this.itemNameLabel1.Name = "itemNameLabel1";
+            this.itemNameLabel1.Size = new System.Drawing.Size(62, 18);
+            this.itemNameLabel1.TabIndex = 3;
+            this.itemNameLabel1.Text = "ItemName1";
+            // 
+            // itemNameLabel2
+            // 
+            this.itemNameLabel2.AutoSize = true;
+            this.itemNameLabel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.itemNameLabel2.Location = new System.Drawing.Point(30, 48);
+            this.itemNameLabel2.Name = "itemNameLabel2";
+            this.itemNameLabel2.Size = new System.Drawing.Size(62, 18);
+            this.itemNameLabel2.TabIndex = 1;
+            this.itemNameLabel2.Text = "ItemName2";
+            // 
+            // itemNameLabel3
+            // 
+            this.itemNameLabel3.AutoSize = true;
+            this.itemNameLabel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.itemNameLabel3.Location = new System.Drawing.Point(30, 72);
+            this.itemNameLabel3.Name = "itemNameLabel3";
+            this.itemNameLabel3.Size = new System.Drawing.Size(62, 19);
+            this.itemNameLabel3.TabIndex = 2;
+            this.itemNameLabel3.Text = "ItemName3";
+            // 
+            // nameValueLabel
+            // 
+            this.nameValueLabel.AutoEllipsis = true;
+            this.nameValueLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nameValueLabel.Location = new System.Drawing.Point(98, 0);
+            this.nameValueLabel.Name = "nameValueLabel";
+            this.nameValueLabel.Size = new System.Drawing.Size(165, 18);
+            this.nameValueLabel.TabIndex = 5;
+            this.nameValueLabel.Text = "名前 or 作者名";
+            this.nameValueLabel.UseMnemonic = false;
+            // 
+            // InfoControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "InfoControl";
+            this.Size = new System.Drawing.Size(266, 91);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label itemNameLabel1;
+        private System.Windows.Forms.Label itemNameLabel2;
+        private System.Windows.Forms.Label itemNameLabel3;
+        private System.Windows.Forms.Label nameValueLabel;
+    }
+}
---
+++ Ragnarok.Forms/Controls/InfoControl.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Ragnarok.Utility;
+
+namespace Ragnarok.Forms.Controls
+{
+    /// <summary>
+    /// InfoBaseクラスの情報を表示するためのコントロールです。
+    /// </summary>
+    public partial class InfoControl : UserControl
+    {
+        private readonly Label[] itemNameLabels;
+        private readonly Label[] itemValueLabels;
+        private InfoBase info;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public InfoControl()
+        {
+            InitializeComponent();
+
+            this.itemNameLabels = new Label[]
+            {
+                this.itemNameLabel1,
+                this.itemNameLabel2,
+                this.itemNameLabel3,
+            };
+            this.itemValueLabels = new Label[3];
+        }
+
+        /// <summary>
+        /// Infoプロパティの変更通知を出します。
+        /// </summary>
+        public event EventHandler InfoChanged;
+
+        /// <summary>
+        /// 情報を表示するInfoBaseオブジェクトを取得します。
+        /// </summary>
+        public InfoBase Info
+        {
+            get { return this.info; }
+            set
+            {
+                if (this.info != value)
+                {
+                    this.info = value;
+
+                    InfoChanged.SafeRaiseEvent(this, EventArgs.Empty);
+                    InfoUpdated();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Infoプロパティが変わった時に呼ばれます。
+        /// </summary>
+        private void InfoUpdated()
+        {
+            try
+            {
+                // いったんレイアウトをすべて停止します。
+                this.tableLayoutPanel.SuspendLayout();
+
+                if (Info == null)
+                {
+                    return;
+                }
+
+                // 名前 or 作者名
+                if (string.IsNullOrEmpty(Info.AuthorName))
+                {
+                    this.nameLabel.Text = "名前：";
+                    this.nameValueLabel.Text = Info.Title;
+                }
+                else
+                {
+                    this.nameLabel.Text = "制作者：";
+                    this.nameValueLabel.Text = Info.AuthorName;
+                }
+
+                var infoItems = GetInfoItems().ToList();
+                for (var i = 0; i < 3; ++i)
+                {
+                    // 前に作ったラベルは消しておきます。
+                    if (this.itemValueLabels[i] != null)
+                    {
+                        this.tableLayoutPanel.Controls.Remove(this.itemValueLabels[i]);
+                        this.itemValueLabels[i] = null;
+                    }
+
+                    if (i < infoItems.Count())
+                    {
+                        var infoItem = infoItems[i];
+                        this.itemNameLabels[i].Text = infoItem.Item1 + "：";
+
+                        // 値の追加
+                        var valueLabel = CreateLabel(infoItem.Item2, infoItem.Item3);
+                        this.tableLayoutPanel.Controls.Add(valueLabel, 1, 2 + i * 2);
+                        this.itemValueLabels[i] = valueLabel;
+                    }
+                    else
+                    {
+                        this.itemNameLabels[i].Text = string.Empty;
+                    }
+                }
+            }
+            finally
+            {
+                // レイアウトの状態は必ず元に戻します。
+                this.tableLayoutPanel.ResumeLayout(false);
+                this.tableLayoutPanel.PerformLayout();
+            }
+        }
+
+        /// <summary>
+        /// コントロールに表示する情報一覧を取得します。
+        /// </summary>
+        private IEnumerable<Tuple<string, string, string>> GetInfoItems()
+        {
+            if (Info.PixivId != 0)
+            {
+                yield return Tuple.Create("pixiv", Info.PixivId.ToString(), info.PixivUrl);
+            }
+
+            if (!string.IsNullOrEmpty(info.NicoCommunity))
+            {
+                yield return Tuple.Create("nicommunity", Info.NicoCommunity, info.NicoCommunityUrl);
+            }
+
+            if (!string.IsNullOrEmpty(info.TwitterId))
+            {
+                yield return Tuple.Create("twitter", Info.TwitterId, info.TwitterUrl);
+            }
+
+            if (!string.IsNullOrEmpty(info.HomepageUrl))
+            {
+                yield return Tuple.Create("website", Info.HomepageUrl, info.HomepageUrl);
+            }
+
+            if (!string.IsNullOrEmpty(info.BlogUrl))
+            {
+                yield return Tuple.Create("blog", Info.BlogUrl, info.BlogUrl);
+            }
+
+            if (!string.IsNullOrEmpty(info.MailAddress))
+            {
+                yield return Tuple.Create("mail", Info.MailAddress, string.Empty);
+            }
+
+            if (!string.IsNullOrEmpty(Info.Comment))
+            {
+                yield return Tuple.Create("comment", Info.Comment, string.Empty);
+            }
+        }
+
+#if false
+        /// <summary>
+        /// 指定のラベル名を持つラベルをレイアウトパネルに追加します。
+        /// </summary>
+        private void AddItem(Tuple<string, string, string> infoItem, int row)
+        {
+            var nameLabel = CreateLabel(infoItem.Item1);
+            var valueLabel = CreateLabel(infoItem.Item2, infoItem.Item3);
+
+            this.tableLayoutPanel.Controls.Add(nameLabel, 0, row);
+            this.tableLayoutPanel.Controls.Add(valueLabel, 1, row);
+        }
+#endif
+
+        /// <summary>
+        /// ラベルかリンク付きラベルを作成します。
+        /// </summary>
+        private Label CreateLabel(string text, string link = "")
+        {
+            if (string.IsNullOrEmpty(link))
+            {
+                return new Label
+                {
+                    Text = text,
+                    Dock = DockStyle.Fill,
+                };
+            }
+            else
+            {
+                var linkLabel = new LinkLabel
+                {
+                    Text = text,
+                    Dock = DockStyle.Fill,
+                };
+
+                linkLabel.Links.Add(0, text.Length, link);
+                linkLabel.LinkClicked += OnHttpLink;
+                return linkLabel;
+            }
+        }
+
+        /// <summary>
+        /// リンクがクリックされたときにリンク先に移動します。
+        /// </summary>
+        private void OnHttpLink(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            e.Link.Visited = true;
+            System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+    }
+}
