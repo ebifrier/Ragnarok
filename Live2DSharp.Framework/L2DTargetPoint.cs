92c244cb79b3d5ba8939fb49ca1352f4ba59113b
ebifrier <ebifrier@gmail.com>
2015-01-05 19:30:16 +0900

    Live2Dモデルがプルプルするバグを修正しました。

--- Live2DSharp.Framework/L2DTargetPoint.cs
+++ Live2DSharp.Framework/L2DTargetPoint.cs
@@ -76,21 +76,18 @@ namespace Live2DSharp.Framework
             private set;
         }
 
-        /***************************************************
-         @brief      更新
-         @brief      首を中央から左右に振るときの平均的な速さは、秒程度。加速・減速を考慮して、その２倍を最高速度とする。
-         @brief      顔のふり具合を、中央(0)から、左右は(±1)とする。
-         @param      なし
-         @return     なし
-         ****************************************************/
+        /// <summary>
+        /// 更新処理を行います。
+        /// </summary>
+        /// <remarks>
+        /// 首を中央から左右に振るときの平均的な速さは、秒程度。
+        /// 加速・減速を考慮して、その２倍を最高速度とする。
+        /// 顔のふり具合を、中央(0)から、左右は(±1)とする。
+        /// </remarks>
         public void Update(TimeSpan elapsed)
         {
-            // 首を中央から左右に振るときの平均的な早さは  秒程度。加速・減速を考慮して、
-            // その２倍を最高速度とする
-            // 顔のふり具合を、中央（０）から、左右は（±１）とする
-
             // 7.5秒間に40分移動（5.3/sc)
-            const double FACE_PARAM_MAX_V = 40.0f / 10;
+            const double FACE_PARAM_MAX_V = 30.0f / 10;
 
             // 1frameあたりに変化できる速度の上限
             const double MAX_V = FACE_PARAM_MAX_V * 1.0f / FRAME_RATE;
@@ -105,26 +102,31 @@ namespace Live2DSharp.Framework
             var MAX_A = deltaTimeWeight * MAX_V / FRAME_TO_MAX_SPEED;
 
             // 目指す向きは、tmpdx,tmpdy 方向のベクトルとなる
-            var dx = (FaceTargetX - FaceCurrentX);
-            var dy = (FaceTargetY - FaceCurrentY);
+            var vx = (FaceTargetX - FaceCurrentX);
+            var vy = (FaceTargetY - FaceCurrentY);
 
             // 変化なし
-            if (dx == 0 && dy == 0)
+            var tolerance = 1E-8;
+            if (MathEx.Compare(vx, 0, tolerance) == 0 &&
+                MathEx.Compare(vy, 0, tolerance) == 0)
             {
                 return;
             }
 
             // 速度の最大よりも大きい場合は、速度を落とす
-            var d = MathEx.InnerProduct(dx, dy);
-            var vx = MAX_V * dx / d;
-            var vy = MAX_V * dy / d;
+            var v = MathEx.InnerProduct(vx, vy);
+            if (v > MAX_V)
+            {
+                vx *= MAX_V / v;
+                vy *= MAX_V / v;
+            }
 
             // 現在の速度から、新規速度への変化（加速度）を求める
             var ax = vx - FaceVelocityX;
             var ay = vy - FaceVelocityY;
 
             var a = MathEx.InnerProduct(ax, ay);
-            if (Math.Abs(a) > MAX_A)
+            if (a > MAX_A)
             {
                 ax *= MAX_A / a;
                 ay *= MAX_A / a;
@@ -150,7 +152,7 @@ namespace Live2DSharp.Framework
                 // 	時刻tは、あらかじめ加速度、速度を1/60(フレームレート、単位なし)で
                 // 	考えているので、t＝１として消してよい（※未検証）
 
-                var term = Math.Sqrt(MAX_A * MAX_A + 16 * MAX_A * d - 8 * MAX_A * d);
+                var term = Math.Sqrt(MAX_A * MAX_A + 16 * MAX_A * v - 8 * MAX_A * v);
                 var max_v = 0.5 * (term - MAX_A);
                 var cur_v = MathEx.InnerProduct(FaceVelocityX, FaceVelocityY);
 
