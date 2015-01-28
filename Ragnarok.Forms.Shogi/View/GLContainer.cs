fedcc98cb18bd44aa921973e1fed09f3f5d74f1b
ebifrier <ebifrier@gmail.com>
2015-01-28 20:17:40 +0900

    不要なDLL参照を削除しました。

--- Ragnarok.Forms.Shogi/View/GLContainer.cs
+++ Ragnarok.Forms.Shogi/View/GLContainer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Drawing;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
