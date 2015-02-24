087584f3b7587d7f5a08f8cb71de40e73f1bf438
ebifrier <ebifrier@gmail.com>
2015-02-25 01:39:45 +0900

    GLEvaluationElementの設定ダイアログのレイアウトを修正しました。

--- Ragnarok.Forms.Shogi/View/GLEvaluationElementSettingDialog.Designer.cs
+++ Ragnarok.Forms.Shogi/View/GLEvaluationElementSettingDialog.Designer.cs
@@ -28,65 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.infoControl = new Ragnarok.Forms.Controls.InfoControl();
-            this.label1 = new System.Windows.Forms.Label();
-            this.imageSetListComboBox = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.valueFullWidthCheckBox = new System.Windows.Forms.CheckBox();
             this.visibleValueCheckBox = new System.Windows.Forms.CheckBox();
             this.cancelButton = new System.Windows.Forms.Button();
             this.okButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.imageSetListComboBox = new System.Windows.Forms.ComboBox();
+            this.infoControl = new Ragnarok.Forms.Controls.InfoControl();
+            this.tableLayoutPanel1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cancelButton, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.okButton, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(3);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(301, 270);
+            this.tableLayoutPanel1.TabIndex = 9;
             // 
             // groupBox2
             // 
+            this.groupBox2.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBox2, 3);
             this.groupBox2.Controls.Add(this.infoControl);
-            this.groupBox2.Location = new System.Drawing.Point(12, 50);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(6, 65);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(301, 101);
-            this.groupBox2.TabIndex = 5;
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.groupBox2.Size = new System.Drawing.Size(289, 87);
+            this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "基本情報";
             // 
-            // infoControl
-            // 
-            this.infoControl.AutoSize = true;
-            this.infoControl.Info = null;
-            this.infoControl.Location = new System.Drawing.Point(6, 18);
-            this.infoControl.Name = "infoControl";
-            this.infoControl.Size = new System.Drawing.Size(289, 77);
-            this.infoControl.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 12);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "選択された評価値画像";
-            // 
-            // imageSetListComboBox
-            // 
-            this.imageSetListComboBox.DisplayMember = "Title";
-            this.imageSetListComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.imageSetListComboBox.FormattingEnabled = true;
-            this.imageSetListComboBox.Location = new System.Drawing.Point(12, 24);
-            this.imageSetListComboBox.Name = "imageSetListComboBox";
-            this.imageSetListComboBox.Size = new System.Drawing.Size(301, 20);
-            this.imageSetListComboBox.TabIndex = 3;
-            // 
             // groupBox1
             // 
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBox1, 3);
             this.groupBox1.Controls.Add(this.valueFullWidthCheckBox);
             this.groupBox1.Controls.Add(this.visibleValueCheckBox);
-            this.groupBox1.Location = new System.Drawing.Point(12, 157);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(6, 162);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(301, 67);
-            this.groupBox1.TabIndex = 6;
+            this.groupBox1.Size = new System.Drawing.Size(289, 67);
+            this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "設定";
             // 
@@ -113,38 +119,68 @@
             // cancelButton
             // 
             this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.cancelButton.Location = new System.Drawing.Point(238, 241);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(220, 239);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(3, 3, 3, 5);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 7;
+            this.cancelButton.TabIndex = 8;
             this.cancelButton.Text = "キャンセル";
             this.cancelButton.UseVisualStyleBackColor = true;
-            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // okButton
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.Location = new System.Drawing.Point(157, 241);
-            this.okButton.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.okButton.Location = new System.Drawing.Point(139, 239);
+            this.okButton.Margin = new System.Windows.Forms.Padding(3, 3, 3, 5);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 8;
+            this.okButton.TabIndex = 9;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
-            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.groupBox3, 3);
+            this.groupBox3.Controls.Add(this.imageSetListComboBox);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(6, 6);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(3, 3, 3, 7);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(289, 49);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "評価値画像表示";
+            // 
+            // imageSetListComboBox
+            // 
+            this.imageSetListComboBox.DisplayMember = "Title";
+            this.imageSetListComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.imageSetListComboBox.FormattingEnabled = true;
+            this.imageSetListComboBox.Location = new System.Drawing.Point(6, 18);
+            this.imageSetListComboBox.Margin = new System.Windows.Forms.Padding(3, 3, 7, 7);
+            this.imageSetListComboBox.Name = "imageSetListComboBox";
+            this.imageSetListComboBox.Size = new System.Drawing.Size(277, 20);
+            this.imageSetListComboBox.TabIndex = 4;
+            // 
+            // infoControl
+            // 
+            this.infoControl.AutoSize = true;
+            this.infoControl.Info = null;
+            this.infoControl.Location = new System.Drawing.Point(6, 20);
+            this.infoControl.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.infoControl.Name = "infoControl";
+            this.infoControl.Size = new System.Drawing.Size(277, 55);
+            this.infoControl.TabIndex = 0;
             // 
             // GLEvaluationElementSettingDialog
             // 
+            this.AcceptButton = this.okButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 273);
-            this.Controls.Add(this.okButton);
-            this.Controls.Add(this.cancelButton);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.imageSetListComboBox);
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(301, 270);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -152,26 +188,29 @@
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "評価値画像の設定";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox imageSetListComboBox;
         private Controls.InfoControl infoControl;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox valueFullWidthCheckBox;
+        private System.Windows.Forms.CheckBox visibleValueCheckBox;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button okButton;
-        private System.Windows.Forms.CheckBox visibleValueCheckBox;
-        private System.Windows.Forms.CheckBox valueFullWidthCheckBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox imageSetListComboBox;
 
     }
 }
