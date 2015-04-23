3cef614ea06198e835c83bd82f52ef3b39cf1a2c
ebifrier <ebifrier@gmail.com>
2015-04-23 23:20:34 +0900

    変数値を文字列に埋め込むためのユーティリティを追加しました。

---
+++ Ragnarok/Utility/StringUtility.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Ragnarok.Utility
+{
+    /// <summary>
+    /// 文字列内への変数埋め込みを実現するためのクラスです。
+    /// </summary>
+    public static class StringUtility
+    {
+        /// <summary>
+        /// '#{name}'で指定された変数名を与えられた辞書の値に置き換えます。
+        /// </summary>
+        public static string NamedFormat<T>(string format,
+                                            IDictionary<string, T> args)
+            where T : class
+        {
+            var replacedFormat = format;
+
+            // string.Format に渡すフォーマット作成
+            // ついでに string.Format に渡す配列も作成
+            var values = args.Select((pair, index) =>
+            {
+                var regex = new Regex(
+                    @"#[{]" + pair.Key + "([^}]*)[}]");
+                Console.WriteLine(regex);
+
+                replacedFormat = regex.Replace(replacedFormat, "{" + index + "$1}");
+                Console.WriteLine(replacedFormat);
+
+                // 配列として使う値一覧を取得します。
+                return pair.Value;
+            }).ToArray();
+
+            // 整形は string.Format にまかせる
+            return string.Format(replacedFormat, values);
+        }
+
+        /// <summary>
+        /// 匿名クラスのインスタンスを渡します。
+        /// </summary>
+        public static string NamedFormat(string format, object obj)
+        {
+            if (string.IsNullOrEmpty(format) || obj == null)
+            {
+                return string.Format(format, obj);
+            }
+
+            // プロパティ名とプロパティ値をセットにした辞書を作り、
+            // それを使って文字列埋め込みを行います。
+            var dic = obj.GetType().GetProperties()
+                .ToDictionary(p => p.Name, p => p.GetValue(obj, null));
+            return NamedFormat(format, dic);
+        }
+    }
+}
---
+++ Ragnarok/Utility/Tests/StringUtilityTest.cs
@@ -0,0 +1,110 @@
+﻿#if TESTS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Ragnarok.Utility.Tests
+{
+    [TestFixture()]
+    public sealed class StringUtilityTest
+    {
+        [Test()]
+        public void DictionaryTest()
+        {
+            // 引数１つ
+            var format = "#{code}";
+            var dic = new Dictionary<string, object>()
+            {
+                { "code", "C1" },
+            };
+            Assert.AreEqual("C1", StringUtility.NamedFormat(format, dic));
+
+            // 引数２つ
+            format = "_#{code} #{22}xx";
+            dic = new Dictionary<string, object>()
+            {
+                { "code", "C1" },
+                { "22", "xyz" },
+            };
+            Assert.AreEqual("_C1 xyzxx", StringUtility.NamedFormat(format, dic));
+
+            // 引数３つ
+            format =
+                "SELECT #{code}, #{name}" +
+                "FROM #{tableName}" +
+                "WHERE #{createdAt} = @createdAt" +
+                "ORDER BY #{code}";
+            dic = new Dictionary<string, object>()
+            {
+                { "tableName", "TBL1" },
+                { "code", "C1" },
+                { "name", "C2" },
+                { "createdAt", "C3" }
+            };
+            Assert.AreEqual(
+                "SELECT C1, C2" +
+                "FROM TBL1" +
+                "WHERE C3 = @createdAt" +
+                "ORDER BY C1",
+                StringUtility.NamedFormat(format, dic));
+        }
+
+        /// <summary>
+        /// オブジェクトを対象としたテストを行います。
+        /// </summary>
+        [Test()]
+        public void ObjectTest()
+        {
+            // プロパティが一つ
+            var format = "#{code}";
+            var obj = new
+            {
+                code = "C1",
+            };
+            Assert.AreEqual("C1", StringUtility.NamedFormat(format, obj));
+
+            // プロパティが２つ
+            var format2 = "_#{Code} #{x33x}xx";
+            var obj2 = new
+            {
+                code = "C1",
+                Code = "C2",
+                x33x = "xyz",
+            };
+            Assert.AreEqual("_C2 xyzxx", StringUtility.NamedFormat(format2, obj2));
+
+            // プロパティが多数
+            var format3 = "SELECT #{code}, #{name}" +
+                "FROM #{tableName}" +
+                "WHERE #{createdAt} = @createdAt" +
+                "ORDER BY #{code}";
+            var obj3 = new
+            {
+                tableName = "TBL1",
+                code = "C1",
+                name = "C2",
+                createdAt = "C3"
+            };
+            Assert.AreEqual(
+                "SELECT C1, C2" +
+                "FROM TBL1" +
+                "WHERE C3 = @createdAt" +
+                "ORDER BY C1",
+                StringUtility.NamedFormat(format3, obj3));
+        }
+
+        /// <summary>
+        /// フォーマット指定子に関するテストを行います。
+        /// </summary>
+        [Test()]
+        public void FormatTest()
+        {
+            var format = "#{Code:0000}";
+            var obj = new { Code = 1 };
+            Assert.AreEqual("0001", StringUtility.NamedFormat(format, obj));
+        }
+    }
+}
+#endif
