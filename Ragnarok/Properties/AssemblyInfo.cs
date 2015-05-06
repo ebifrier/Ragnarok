2f20f2cec9b8419e367cd8257e382b69b3c6b6fd
ebifrier <ebifrier@gmail.com>
2015-05-06 23:16:54 +0900

    バージョンを3.0.0.0に更新しました。

--- Ragnarok/Properties/AssemblyInfo.cs
+++ Ragnarok/Properties/AssemblyInfo.cs
@@ -65,4 +65,4 @@ using System.Windows.Markup;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
