794ef95bf13d646d33812e4c36312510bbd04c0b
ebifrier <ebifrier@gmail.com>
2016-07-30 02:39:20 +0900

    バージョン表示用のダイアログを追加

---
+++ Ragnarok.Forms/Controls/VersionForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Ragnarok.Forms.Controls
+{
+    partial class VersionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.okButton = new System.Windows.Forms.Button();
+            this.appNameLabel = new System.Windows.Forms.Label();
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.copyrightLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(197, 155);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 0;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // appNameLabel
+            // 
+            this.appNameLabel.AutoSize = true;
+            this.appNameLabel.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.appNameLabel.Location = new System.Drawing.Point(15, 13);
+            this.appNameLabel.Name = "appNameLabel";
+            this.appNameLabel.Size = new System.Drawing.Size(154, 35);
+            this.appNameLabel.TabIndex = 1;
+            this.appNameLabel.Text = "AppName";
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.versionLabel.Location = new System.Drawing.Point(16, 59);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Size = new System.Drawing.Size(106, 27);
+            this.versionLabel.TabIndex = 2;
+            this.versionLabel.Text = "Version ";
+            // 
+            // copyrightLabel
+            // 
+            this.copyrightLabel.AutoSize = true;
+            this.copyrightLabel.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.copyrightLabel.Location = new System.Drawing.Point(18, 101);
+            this.copyrightLabel.Name = "copyrightLabel";
+            this.copyrightLabel.Size = new System.Drawing.Size(70, 16);
+            this.copyrightLabel.TabIndex = 3;
+            this.copyrightLabel.Text = "copyright";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.appNameLabel);
+            this.panel1.Controls.Add(this.copyrightLabel);
+            this.panel1.Controls.Add(this.versionLabel);
+            this.panel1.Location = new System.Drawing.Point(3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(276, 145);
+            this.panel1.TabIndex = 4;
+            // 
+            // VersionForm
+            // 
+            this.AcceptButton = this.okButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 186);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.okButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VersionForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "バージョン表示";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label appNameLabel;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.Label copyrightLabel;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Ragnarok.Forms/Controls/VersionForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Ragnarok.Utility.AssemblyUtility;
+
+namespace Ragnarok.Forms.Controls
+{
+    public partial class VersionForm : Form
+    {
+        private AssemblyAccessor assemblyAccessor;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public VersionForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// アセンブリ情報を取得または設定します。
+        /// </summary>
+        public AssemblyAccessor AssemblyAccessor
+        {
+            get { return this.assemblyAccessor; }
+            set
+            {
+                if (this.assemblyAccessor != value)
+                {
+                    this.assemblyAccessor = value;
+
+                    OnAssemblyAccessorChanged();
+                }
+            }
+        }
+
+        private void OnAssemblyAccessorChanged()
+        {
+            FormsUtil.UIProcess(() =>
+            {
+                this.appNameLabel.Text = AssemblyAccessor?.Title;
+                this.versionLabel.Text = "Version " + AssemblyAccessor?.Version;
+                this.copyrightLabel.Text = AssemblyAccessor?.Copyright;
+            });
+        }
+    }
+}
