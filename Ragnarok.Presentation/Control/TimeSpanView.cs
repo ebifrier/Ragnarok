36305252efa4ca20720b51a9a41060826a01d40e
ebifrier <ebifrier@gmail.com>
2017-11-09 08:54:40 +0900

    Presentation用の時間表示を修正

--- Ragnarok.Presentation/Control/TimeSpanView.cs
+++ Ragnarok.Presentation/Control/TimeSpanView.cs
@@ -22,37 +22,20 @@ namespace Ragnarok.Presentation.Control
     /// 文字位置がずれてしまいます。
     /// </remarks>
     [TemplatePart(Name = "Part_NormalContainer", Type = typeof(UIElement))]
-    [TemplatePart(Name = "Part_ValueText0", Type = typeof(DecoratedText))]
-    [TemplatePart(Name = "Part_ValueText1", Type = typeof(DecoratedText))]
-    [TemplatePart(Name = "Part_ValueText2", Type = typeof(DecoratedText))]
-    [TemplatePart(Name = "Part_ValueText3", Type = typeof(DecoratedText))]
-    [TemplatePart(Name = "Part_ValueText4", Type = typeof(DecoratedText))]
-    [TemplatePart(Name = "Part_ValueText5", Type = typeof(DecoratedText))]
-    [TemplatePart(Name = "Part_ColonText0", Type = typeof(DecoratedText))]
-    [TemplatePart(Name = "Part_ColonText1", Type = typeof(DecoratedText))]
+    [TemplatePart(Name = "Part_ValueText", Type = typeof(DecoratedText))]
     [TemplatePart(Name = "Part_SpecialContainer", Type = typeof(UIElement))]
     [TemplatePart(Name = "Part_StringText", Type = typeof(DecoratedText))]
     public partial class TimeSpanView : DecoratedTextBase
     {
         private readonly string NormalContainerName = "Part_NormalContainer";
+        private readonly string ValueTextName = "Part_ValueText";
         private readonly string SpecialContainerName = "Part_SpecialContainer";
         private readonly string StringTextName = "Part_StringText";
 
         private UIElement normalContainer;
-        private DecoratedText[] valueTexts = new DecoratedText[6];
-        private DecoratedText[] colonTexts = new DecoratedText[2];
+        private DecoratedText valueText;
         private UIElement specialContainer;
         private DecoratedText stringText;
-
-        private static string Part_ValueTextName(int n)
-        {
-            return string.Format("Part_ValueText{0}", n);
-        }
-
-        private static string Part_ColonTextName(int n)
-        {
-            return string.Format("Part_ColonText{0}", n);
-        }
 
         /// <summary>
         /// 静的コンストラクタ
@@ -62,28 +45,6 @@ namespace Ragnarok.Presentation.Control
             DefaultStyleKeyProperty.OverrideMetadata(
                 typeof(TimeSpanView),
                 new FrameworkPropertyMetadata(typeof(TimeSpanView)));
-            FontSizeProperty.OverrideMetadata(
-                typeof(TimeSpanView),
-                new FrameworkPropertyMetadata(OnFontSizeChanged));
-        }
-
-        private static void OnFontSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var self = (TimeSpanView)d;
-
-            self.NumberWidth =
-                self.FontSize / 2.0 +
-                Math.Max(1.0, self.FontSize / 10);
-        }
-
-        /// <summary>
-        /// コンストラクタ
-        /// </summary>
-        public TimeSpanView()
-        {
-            OnFontSizeChanged(this,
-                new DependencyPropertyChangedEventArgs(
-                    FontSizeProperty, FontSize, FontSize));
         }
 
         /// <summary>
@@ -101,23 +62,6 @@ namespace Ragnarok.Presentation.Control
         {
             get { return (TimeSpan)GetValue(ValueProperty); }
             set { SetValue(ValueProperty, value); }
-        }
-
-        /// <summary>
-        /// 各数字の表示幅を扱う依存プロパティです。
-        /// </summary>
-        public static readonly DependencyProperty NumberWidthProperty =
-            DependencyProperty.Register(
-                "NumberWidth", typeof(double), typeof(TimeSpanView),
-                new FrameworkPropertyMetadata(7.0));
-
-        /// <summary>
-        /// 各数字の表示幅を取得または設定します。
-        /// </summary>
-        public double NumberWidth
-        {
-            get { return (double)GetValue(NumberWidthProperty); }
-            set { SetValue(NumberWidthProperty, value); }
         }
 
         /// <summary>
@@ -205,48 +149,12 @@ namespace Ragnarok.Presentation.Control
         /// </summary>
         private void UpdateValueTexts(TimeSpan span)
         {
-            for (var i = 0; i < this.valueTexts.Length; ++i)
+            if (this.valueText == null)
             {
-                var control = this.valueTexts[i];
-                if (control == null)
-                {
-                    continue;
-                }
-
-                var n = 0;
-                switch (i)
-                {
-                    case 0: n = span.Seconds; break;
-                    case 1: n = span.Seconds / 10; break;
-                    case 2: n = span.Minutes; break;
-                    case 3: n = span.Minutes / 10; break;
-                    case 4: n = span.Hours; break;
-                    case 5: n = span.Hours / 10; break;
-                }
-
-                control.Text = (n % 10).ToString();
+                return;
             }
 
-            // ':'記号の不透明度。
-            // 静止状態では表示したいため、ミリ秒が０の時は
-            // 不透明度を1.0にする必要があります。
-            /*var millis = (span.Milliseconds < 500 ?
-                span.Milliseconds :
-                1000 - span.Milliseconds);
-            var opacity = MathEx.InterpLiner(1.0, 0.0, millis * 0.002);
-            Log.Info("opacity: {0}, {1}", span.Milliseconds, opacity);*/
-
-            // ':'記号の不透明度を変更します。
-            for (var i = 0; i < this.colonTexts.Length; ++i)
-            {
-                var control = this.colonTexts[i];
-                if (control == null)
-                {
-                    continue;
-                }
-
-                //control.Opacity = opacity;
-            }
+            this.valueText.Text = span.ToString(@"hh\:mm\:ss");
         }
 
         /// <summary>
@@ -257,18 +165,9 @@ namespace Ragnarok.Presentation.Control
             base.OnApplyTemplate();
 
             this.normalContainer = (UIElement)GetTemplateChild(NormalContainerName);
+            this.valueText = (DecoratedText)GetTemplateChild(ValueTextName);
             this.specialContainer = (UIElement)GetTemplateChild(SpecialContainerName);
             this.stringText = (DecoratedText)GetTemplateChild(StringTextName);
-
-            for (var i = 0; i < this.valueTexts.Length; ++i)
-            {
-                this.valueTexts[i] = (DecoratedText)GetTemplateChild(Part_ValueTextName(i));
-            }
-
-            for (var i = 0; i < this.colonTexts.Length; ++i)
-            {
-                this.colonTexts[i] = (DecoratedText)GetTemplateChild(Part_ColonTextName(i));
-            }
 
             ValueUpdated(Value);
         }
