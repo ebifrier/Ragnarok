4652267731ec3f1016d57079039086e299dcdfd7
ebifrier <ebifrier@gmail.com>
2020-05-30 17:28:07 +0900

    ShogiParserのテストを追加

---
+++ Ragnarok.Shogi/Tests/ShogiParserTest.cs
@@ -0,0 +1,235 @@
+﻿#if TESTS
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Ragnarok.Shogi.Tests
+{
+    [TestFixture()]
+    public sealed class ShogiParserTest
+    {
+        [Test()]
+        public void ResignTest()
+        {
+            var resign = new LiteralMove
+            {
+                SpecialMoveType = SpecialMoveType.Resign,
+            };
+
+            Assert.AreEqual(
+                ShogiParser.ParseMove("投了", true),
+                resign);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("TORYO", true),
+                resign);
+            Assert.Null(ShogiParser.ParseMove("まけました", true));
+        }
+
+        [Test()]
+        public void ParseMoveTest()
+        {
+            Assert.AreEqual(
+                ShogiParser.ParseMove("同角", true),
+                new LiteralMove
+                {
+                    SameAsOld = true,
+                    Piece = Piece.Kaku,
+                });
+
+            var gin68 = new LiteralMove
+            {
+                DstSquare = new Square(6, 8),
+                Piece = Piece.Gin,
+            };
+            Assert.AreEqual(
+                ShogiParser.ParseMove("68銀", true),
+                gin68);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("６８銀", true),
+                gin68);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("六八銀", true),
+                gin68);
+
+            Assert.AreEqual(
+                ShogiParser.ParseMove("４６歩", true),
+                new LiteralMove
+                {
+                    DstSquare = new Square(4, 6),
+                    Piece = Piece.Hu,
+                });
+            Assert.AreEqual(
+                ShogiParser.ParseMove("３２金", true),
+                new LiteralMove
+                {
+                    DstSquare = new Square(3, 2),
+                    Piece = Piece.Kin,
+                });
+
+            Assert.AreEqual(
+                ShogiParser.ParseMove("ろくはち銀", true),
+                null);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("56ふ", true),
+                null);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("救急玉", true),
+                null);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("32RYUU", true),
+                null);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("32キン", true),
+                null);
+        }
+
+        [Test()]
+        public void ParseMoveTest2()
+        {
+            Assert.AreEqual(
+                ShogiParser.ParseMove("１３馬右", true),
+                new LiteralMove
+                {
+                    DstSquare = new Square(1, 3),
+                    Piece = Piece.Uma,
+                    RelFileType = RelFileType.Right,
+                });
+            Assert.AreEqual(
+                ShogiParser.ParseMove("１３馬左引", true),
+                new LiteralMove
+                {
+                    DstSquare = new Square(1, 3),
+                    Piece = Piece.Uma,
+                    RankMoveType = RankMoveType.Back,
+                    RelFileType = RelFileType.Left,
+                });
+            Assert.AreEqual(
+                ShogiParser.ParseMove("１３歩不成", true),
+                new LiteralMove
+                {
+                    DstSquare = new Square(1, 3),
+                    Piece = Piece.Hu,
+                    ActionType = ActionType.Unpromote,
+                });
+            Assert.AreEqual(
+                ShogiParser.ParseMove("５５馬", true),
+                new LiteralMove
+                {
+                    DstSquare = new Square(5, 5),
+                    Piece = Piece.Uma,
+                });
+             Assert.AreEqual(
+                ShogiParser.ParseMove("△６二角上", true),
+                new LiteralMove
+                {
+                    Piece = Piece.Kaku,
+                    DstSquare = new Square(6, 2),
+                    RankMoveType = RankMoveType.Up,
+                    BWType = BWType.White,
+                });
+
+            Assert.AreEqual(
+                ShogiParser.ParseMove("43ほーす左", true),
+                null);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("３９ときんちゃん", true),
+                null);
+            Assert.AreEqual(
+                ShogiParser.ParseMove("ごよんぽ", true),
+                null);
+        }
+
+        [Test()]
+        public void ParseSameAsTest()
+        {
+            Assert.AreEqual(
+                new LiteralMove
+                {
+                    SameAsOld = true,
+                    Piece = Piece.Hu,
+                },
+                ShogiParser.ParseMove("同歩", true));
+            Assert.AreEqual(
+                new LiteralMove
+                {
+                    SameAsOld = true,
+                    Piece = Piece.Ryu,
+                },
+                ShogiParser.ParseMove("同　竜", true));
+            Assert.AreEqual(
+                new LiteralMove
+                {
+                    SameAsOld = true,
+                    Piece = Piece.NariGin,
+                    RankMoveType = RankMoveType.Up,
+                    RelFileType = RelFileType.Left,
+                },
+                ShogiParser.ParseMove("同成銀左上", true));
+
+            Assert.AreEqual(
+                null,
+                ShogiParser.ParseMove("DOU　馬", true));
+            Assert.AreEqual(
+                null,
+                ShogiParser.ParseMove("同衾右", true));
+            Assert.AreEqual(
+               null,
+               ShogiParser.ParseMove("34同じくおふーさん", true));
+            Assert.AreEqual(
+               null,
+               ShogiParser.ParseMove("34同 ぎん右上がる", true));
+            Assert.AreEqual(
+               null,
+               ShogiParser.ParseMove("99dou kinghidarisagaru", true));
+        }
+
+        /// <summary>
+        /// 投了や中断などの特殊な指し手のパーステストを行います。
+        /// </summary>
+        [Test()]
+        public void ParseSpecialMoveTest()
+        {
+            Assert.AreEqual(
+                new LiteralMove
+                {
+                    SpecialMoveType = SpecialMoveType.Resign,
+                },
+                ShogiParser.ParseMove("投了", true));
+            Assert.AreEqual(
+                new LiteralMove
+                {
+                    SpecialMoveType = SpecialMoveType.Interrupt,
+                    BWType = BWType.Black,
+                },
+                ShogiParser.ParseMove("▲中断", true));
+            Assert.AreEqual(
+                new LiteralMove
+                {
+                    SpecialMoveType = SpecialMoveType.Sennichite,
+                    BWType = BWType.White,
+                },
+                ShogiParser.ParseMove("△千日手", true));
+            Assert.AreEqual(
+                new LiteralMove
+                {
+                    SpecialMoveType = SpecialMoveType.TimeUp,
+                    BWType = BWType.Black,
+                },
+                ShogiParser.ParseMove("▼時間切れ", true));
+            Assert.AreEqual(
+                new LiteralMove
+                {
+                    SpecialMoveType = SpecialMoveType.Jishogi
+                },
+                ShogiParser.ParseMove("持将棋X", false));
+
+            Assert.Null(ShogiParser.ParseMove("▽とうりょう", true));
+            Assert.Null(ShogiParser.ParseMove("不明", true));
+            Assert.Null(ShogiParser.ParseMove("投 了", true));
+            Assert.Null(ShogiParser.ParseMove("▲投了A", true));
+        }
+    }
+}
+#endif
