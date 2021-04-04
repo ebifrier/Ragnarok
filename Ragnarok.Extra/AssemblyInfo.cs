f061cbd8feebe51dde263c6bad28a3b47ace8cd4
ebifrier <ebifrier@gmail.com>
2021-04-04 23:03:27 +0900

    XmnlnsDefinitionを追加し、Xamlからオブジェクトを作成できるようにした

--- Ragnarok.Extra/AssemblyInfo.cs
+++ Ragnarok.Extra/AssemblyInfo.cs
@@ -27,3 +27,12 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Extra.Sound")]
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Extra.Xaml")]
 #endif
+
+[assembly: XmlnsDefinition("http://schemas.flint-sharp/xaml", "FlintSharp.Behaviours")]
+[assembly: XmlnsDefinition("http://schemas.flint-sharp/xaml", "FlintSharp.Activities")]
+[assembly: XmlnsDefinition("http://schemas.flint-sharp/xaml", "FlintSharp.Counters")]
+[assembly: XmlnsDefinition("http://schemas.flint-sharp/xaml", "FlintSharp.Easing")]
+[assembly: XmlnsDefinition("http://schemas.flint-sharp/xaml", "FlintSharp.Emitters")]
+[assembly: XmlnsDefinition("http://schemas.flint-sharp/xaml", "FlintSharp.Initializers")]
+[assembly: XmlnsDefinition("http://schemas.flint-sharp/xaml", "FlintSharp.Particles")]
+[assembly: XmlnsDefinition("http://schemas.flint-sharp/xaml", "FlintSharp.Zones")]
---
+++ Ragnarok.OpenGL/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml", "Ragnarok.OpenGL")]
