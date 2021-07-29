27d2f2369453c51ce47e46fb3a99658e2bbdbb9e
ebifrier <ebifrier@gmail.com>
2021-07-29 21:01:29 +0900

    Ragnarok.PresentationにThemeInfoを追加

--- Ragnarok.Presentation/AssemblyInfo.cs
+++ Ragnarok.Presentation/AssemblyInfo.cs
@@ -1,4 +1,17 @@
-﻿using System.Windows.Markup;
+﻿using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(
+    //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page, 
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.None,
+
+    //where the generic resource dictionary is located
+    //(used if a resource is not found in the page, 
+    // app, or any theme specific resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly
+)]
 
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation")]
 [assembly: XmlnsDefinition("http://schemas.garnet-alice.net/ragnarok/xaml/presentation", "Ragnarok.Presentation.Command")]
