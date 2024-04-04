35952d158fed165e46671447d08f1df6952fcec2
ebifrier <ebifrier@gmail.com>
2024-04-05 00:37:02 +0900

    MovableFormのデフォルトのコンテキストメニューを、派生クラスが操作できるようにprotected属性にした

--- Ragnarok.Forms/Controls/MovableForm.Designer.cs
+++ Ragnarok.Forms/Controls/MovableForm.Designer.cs
@@ -28,68 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.maximumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.contextMenuStrip.SuspendLayout();
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(components);
+            maximumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            contextMenuStrip.SuspendLayout();
+            SuspendLayout();
             // 
             // contextMenuStrip
             // 
-            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.maximumToolStripMenuItem,
-            this.normalToolStripMenuItem,
-            this.toolStripSeparator1,
-            this.closeToolStripMenuItem});
-            this.contextMenuStrip.Name = "contextMenuStrip";
-            this.contextMenuStrip.Size = new System.Drawing.Size(181, 98);
+            contextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { maximumToolStripMenuItem, normalToolStripMenuItem, toolStripSeparator1, closeToolStripMenuItem });
+            contextMenuStrip.Name = "contextMenuStrip";
+            contextMenuStrip.Size = new System.Drawing.Size(211, 110);
             // 
             // maximumToolStripMenuItem
             // 
-            this.maximumToolStripMenuItem.Name = "maximumToolStripMenuItem";
-            this.maximumToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.maximumToolStripMenuItem.Text = "最大化(&M)";
+            maximumToolStripMenuItem.Name = "maximumToolStripMenuItem";
+            maximumToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            maximumToolStripMenuItem.Text = "最大化(&M)";
             // 
             // normalToolStripMenuItem
             // 
-            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
-            this.normalToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.normalToolStripMenuItem.Text = "通常表示(&N)";
+            normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            normalToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            normalToolStripMenuItem.Text = "通常表示(&N)";
             // 
             // toolStripSeparator1
             // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new System.Drawing.Size(207, 6);
             // 
             // closeToolStripMenuItem
             // 
-            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.closeToolStripMenuItem.Text = "閉じる(&X)";
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            closeToolStripMenuItem.Text = "閉じる(&X)";
             // 
             // MovableForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.ContextMenuStrip = this.contextMenuStrip;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "MovableForm";
-            this.Text = "MovableForm";
-            this.contextMenuStrip.ResumeLayout(false);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1067, 750);
+            ContextMenuStrip = contextMenuStrip;
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            Name = "MovableForm";
+            Text = "MovableForm";
+            contextMenuStrip.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
-
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
         private System.Windows.Forms.ToolStripMenuItem maximumToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        protected System.Windows.Forms.ContextMenuStrip contextMenuStrip;
     }
 }
